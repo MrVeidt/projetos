610284438dc0c8cd15470a3daacea260759b69ba
Krsna Pinheiro de Souza Gusmao <krsna.pinheiro@ufsc.br>
2023-08-16 11:53:51 -0300

    Commit da aula de 16/08

---
+++ react-basico/src/components/Component01.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Component01 = () => {
+  return (
+    <div>
+      <h2 className="Título do componente">Meu primeiro Componente React</h2>
+      {console.log("Mensagem de teste")}
+    </div>
+  );
+};
+
+export default Component01;
